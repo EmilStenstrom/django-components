22b00a4a62a749e9aaa424c90faf05a53de43045
Emil Stenström <em@kth.se>
2015-06-11 20:40:47 +0200

    Simple test runner that discovers tests.

---
+++ run_tests.py
@@ -0,0 +1,8 @@
+import unittest
+
+# Runs all tests found in filenames matching "test*""
+if __name__ == '__main__':
+    test_loader = unittest.defaultTestLoader
+    test_runner = unittest.TextTestRunner()
+    test_suite = test_loader.discover('.')
+    test_runner.run(test_suite)
