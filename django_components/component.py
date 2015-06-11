df22a4049f1beefe847282952dd38af58e7cb618
Emil Stenström <em@kth.se>
2015-06-11 21:11:13 +0200

    Move registry to separate file.

--- django_components/component.py
+++ django_components/component.py
@@ -1,24 +1,4 @@
-class AlreadyRegistered(Exception):
-    pass
-
-class NotRegistered(Exception):
-    pass
-
-class ComponentRegistry(object):
-    def __init__(self):
-        self._registry = {}  # component name -> component_class mapping
-
-    def register(self, name=None, component=None):
-        if name in self._registry:
-            raise AlreadyRegistered('The component "%s" is already registered' % name)
-
-        self._registry[name] = component
-
-    def unregister(self, name):
-        if name not in self._registry:
-            raise NotRegistered('The component "%s" is not registered' % name)
-
-        del self._registry[name]
+from .component_registry import ComponentRegistry, AlreadyRegistered, NotRegistered  # NOQA
 
 # This variable represents the global component registry
 registry = ComponentRegistry()
