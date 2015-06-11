972f99af05adc0e8289495e90c06b39ef3011bc3
Emil Stenström <em@kth.se>
2015-06-11 21:28:50 +0200

    Make test slightly more interesting.

--- tests/test_component.py
+++ tests/test_component.py
@@ -1,14 +1,23 @@
 import unittest
 from django_components import component
 
+class SimpleComponent(component.Component):
+    def context(self, variable):
+        return {
+            "variable": variable,
+        }
+
+    class Media:
+        template = "template.html"
+        css = {"all": "style.css"}
+        js = ("script.js",)
+
 class ComponentRegistryTest(unittest.TestCase):
     def test_simple_component(self):
-        class MyComponent(component.Component):
-            pass
 
-        comp = MyComponent()
+        comp = SimpleComponent()
 
-        self.assertEqual(comp.context(), {})
-        self.assertEqual(comp._media.template, None)
-        self.assertEqual(comp._media.css, {})
-        self.assertEqual(comp._media.js, ())
+        self.assertEqual(comp.context("test"), {"variable": "test"})
+        self.assertEqual(comp._media.template, "template.html")
+        self.assertEqual(comp._media.css, {"all": "style.css"})
+        self.assertEqual(comp._media.js, ("script.js",))
