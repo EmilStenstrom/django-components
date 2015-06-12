bc2950e1b43b272b2e747bfb88663b0049bc436f
Ryan Hiebert <ryan@ryanhiebert.com>
2015-06-12 09:39:14 -0500

    Use tox to test

--- run_tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-# Runs all tests found in filenames matching "test*""
-if __name__ == '__main__':
-    test_loader = unittest.defaultTestLoader
-    test_runner = unittest.TextTestRunner()
-    test_suite = test_loader.discover('.')
-    test_runner.run(test_suite)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-components',
+    packages=find_packages(exclude='tests'),
+    classifiers=[
+        'Private :: Do Not Upload',
+    ],
+    install_requires=[
+        'Django>=1.5'
+    ],
+)
