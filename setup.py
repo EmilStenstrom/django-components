de59992288151466305ea660716319c6cfb5459b
Emil Stenström <em@kth.se>
2020-02-08 10:09:28 +0100

    Add more metadata about project.

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
-from setuptools import find_packages, setup
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup
+
+VERSION = '0.1'
 
 setup(
-    name='django-components',
-    packages=find_packages(exclude='tests'),
-    classifiers=[
-        'Private :: Do Not Upload',
-    ],
+    name='django_components',
+    packages=["django_components"],
+    version=VERSION,
+    description='A way to create simple reusable template components in Django.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    long_description_content_type="text/markdown",
+    author=u'Emil Stenström',
+    author_email='em@kth.se',
+    url='https://github.com/EmilStenstrom/django_components/',
     install_requires=[
-        'Django>=1.7'
+        "Django>=1.11",
+        "six",
+    ],
+    keywords=['django', 'components', 'css', 'js', 'html'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
     ],
 )
