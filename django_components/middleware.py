dab69fce25fbea89682ecb4778ea2da492bd43c8
rbeard0330 <@dul2k3BKW6m>
2021-01-31 21:30:56 -0500

    Modify middleware to use process_template_response hook

--- django_components/middleware.py
+++ django_components/middleware.py
@@ -7,30 +7,31 @@ CSS_DEPENDENCY_PLACEHOLDER = '<link name="CSS_PLACEHOLDER" href="#">'
 JS_DEPENDENCY_PLACEHOLDER = '<src name="JS_PLACEHOLDER" href="#">'
 
 
-def component_dependency_middleware_factory(get_response):
-    dependency_regex_value_as_bytes = bytes('{}|{}'.format(CSS_DEPENDENCY_PLACEHOLDER, JS_DEPENDENCY_PLACEHOLDER),
-                                            encoding='utf-8')
-    dependency_regex = re.compile(dependency_regex_value_as_bytes)
+class ComponentDependencyMiddleware:
+    """Middleware that inserts CSS/JS dependencies for all rendered components at points marked with template tags."""
 
-    def component_dependency_middleware(request):
-        response = get_response(request)
-        assert response.is_rendered, "Component dependency middleware received unrendered response"
+    dependency_regex = re.compile(bytes('{}|{}'.format(CSS_DEPENDENCY_PLACEHOLDER, JS_DEPENDENCY_PLACEHOLDER),
+                                        encoding='utf-8'))
 
-        if not hasattr(response, 'add_post_render_callback'):
-            raise TypeError('Component dependency middleware expected response to have add_post_render_callback')
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+
+    def process_template_response(self, _request, response):
+        response.context_data[RENDERED_COMPONENTS_CONTEXT_KEY] = set()
 
         def component_dependency_callback(rendered_response):
             rendered_components = rendered_response.context_data.get(RENDERED_COMPONENTS_CONTEXT_KEY, [])
             required_media = join_media(rendered_components)
 
-            replacer = DependencyReplacer(required_media.render_css(), required_media.render_js(),
-                                          response_encoding=rendered_response.charset)
-            response.content = re.sub(dependency_regex, replacer, response.content)
+            replacer = DependencyReplacer(required_media.render_css(), required_media.render_js())
+            response.content = re.sub(self.dependency_regex, replacer, response.content)
 
         response.add_post_render_callback(component_dependency_callback)
-        return response
 
-    return component_dependency_middleware
+        return response
 
 
 class DependencyReplacer:
@@ -40,24 +41,25 @@ class DependencyReplacer:
     CSS_PLACEHOLDER = bytes(CSS_DEPENDENCY_PLACEHOLDER, encoding='utf-8')
     JS_PLACEHOLDER = bytes(JS_DEPENDENCY_PLACEHOLDER, encoding='utf-8')
 
-    def __init__(self, css_string, js_string, response_encoding='utf-8'):
-        self.response_encoding = response_encoding
-        self.js_string = js_string
-        self.css_string = css_string
+    def __init__(self, css_string, js_string):
+        self.js_string = self.encode(js_string)
+        self.css_string = self.encode(css_string)
 
     def __call__(self, match):
         if match[0] == self.CSS_PLACEHOLDER:
-            replacement = self.css_string
-            self.css_string = b""
-            return self.encode(replacement)
+            replacement, self.css_string = self.css_string, b""
         elif match[0] == self.JS_PLACEHOLDER:
-            replacement = self.js_string
-            self.js_string = b""
-            return self.encode(replacement)
-        raise AssertionError('Invalid match for DependencyReplacer' + match)
+            replacement, self.js_string = self.js_string, b""
+        else:
+            raise AssertionError('Invalid match for DependencyReplacer' + match)
+        return replacement
 
-    def encode(self, s):
-        return bytes(s, encoding=self.response_encoding)
+    @staticmethod
+    def encode(s):
+        """Encode iterable of strings as a single UTF-8 bytes object."""
+
+        s = ''.join(s)
+        return bytes(s, encoding="utf-8")
 
 
 def join_media(components):
