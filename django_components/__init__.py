349998a49645367ab0c6515a19286b368a835afc
Dan Jacob <danjac2018@gmail.com>
2021-02-04 12:04:08 +0200

    import_module, not import_string

--- django_components/__init__.py
+++ django_components/__init__.py
@@ -1,13 +1,13 @@
-from django.utils.module_loading import autodiscover_modules, import_string
+from importlib import import_module
+
+from django.utils.module_loading import autodiscover_modules
 
 
 def autodiscover():
     # look for "components" module/pkg in each app
     from . import app_settings
 
-    print("running autodiscover", app_settings.AUTODISCOVER, app_settings.LIBRARIES)
-
     if app_settings.AUTODISCOVER:
         autodiscover_modules("components")
     for path in app_settings.LIBRARIES:
-        import_string(path)
+        import_module(path)
