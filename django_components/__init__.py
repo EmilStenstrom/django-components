2d32f93a75adad304eb79c6c323160f944994899
Dan Jacob <danjac2018@gmail.com>
2021-02-04 02:41:26 +0200

    Move import into function

--- django_components/__init__.py
+++ django_components/__init__.py
@@ -1,10 +1,10 @@
 from django.utils.module_loading import autodiscover_modules, import_string
 
-from . import app_settings
-
 
 def autodiscover():
     # look for "components" module/pkg in each app
+    from . import app_settings
+
     if app_settings.AUTODISCOVER:
         autodiscover_modules("components")
     for path in app_settings.LIBRARIES:
