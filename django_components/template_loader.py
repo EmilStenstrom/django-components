c1d090d5c0595a44d7656e53ce40a07f3cdaa0ee
Real-Gecko <Real-Gecko@users.noreply.github.com>
2021-08-31 02:51:03 +0600

    Added template loader for component templates automatic discovery (thanks @Real-Gecko)

---
+++ django_components/template_loader.py
@@ -0,0 +1,11 @@
+"""
+Template loader that loads templates from each Django app's "components" directory.
+"""
+
+from django.template.loaders.filesystem import Loader as FilesystemLoader
+from django.template.utils import get_app_template_dirs
+
+
+class Loader(FilesystemLoader):
+    def get_dirs(self):
+        return get_app_template_dirs('components')
