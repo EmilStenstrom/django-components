58c6e476f1da7a227512f360946fc8f4f88796eb
Emil Stenström <emil@emilstenstrom.se>
2022-07-17 13:26:27 +0200

    Automitically load component templates from directory where settings.py files is (old style), and from settings.py parent dir (new style).

--- django_components/template_loader.py
+++ django_components/template_loader.py
@@ -2,10 +2,25 @@
 Template loader that loads templates from each Django app's "components" directory.
 """
 
+from pathlib import Path
+
+from django.conf import settings
 from django.template.loaders.filesystem import Loader as FilesystemLoader
 from django.template.utils import get_app_template_dirs
 
 
 class Loader(FilesystemLoader):
     def get_dirs(self):
-        return get_app_template_dirs("components")
+        component_dir = "components"
+        directories = list(get_app_template_dirs(component_dir))
+        settings_path = Path(*settings.SETTINGS_MODULE.split("."))
+
+        if (path := (settings_path / ".." / component_dir).resolve()).is_dir():
+            directories.append(path)
+
+        if (
+            path := (settings_path / ".." / ".." / component_dir).resolve()
+        ).is_dir():
+            directories.append(path)
+
+        return directories
