b2a328d9abf9f263a23479e80465b639fb0a4b91
Emil Stenström <emil@emilstenstrom.se>
2022-12-13 12:30:56 +0100

    Add *args and **kwargs to make typecheckers and linters happy.

--- django_components/component.py
+++ django_components/component.py
@@ -55,7 +55,7 @@ class Component(metaclass=SimplifiedInterfaceMediaDefiningClass):
         self.instance_template = None
         self.slots = {}
 
-    def get_context_data(self):
+    def get_context_data(self, *args, **kwargs):
         return {}
 
     def get_template_name(self, context=None):
