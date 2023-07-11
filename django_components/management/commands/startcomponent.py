5c1f4bd4380072894ed56423b6d1e92be02614fd
Hanif Birgani <hanifbirgani@gmail.com>
2023-07-11 23:36:31 +0330

    fix(management): change a line to pass flake8. related #249

--- django_components/management/commands/startcomponent.py
+++ django_components/management/commands/startcomponent.py
@@ -58,7 +58,6 @@ from django_components import component
 
 @component.register("{name}")
 class {name.capitalize()}(component.Component):
-    # Templates inside `[your apps]/components` dir and `[project root]/components` dir will be automatically found. To customize which template to use based on context
     # you can override def get_template_name() instead of specifying the below variable.
     template_name = "{name}/template.html"
 
