fd5ea68737e7fc7a6150aa9d91ff947e99f176a7
Juro Oravec <juraj.oravec.josefson@gmail.com>
2024-03-09 10:28:08 +0100

    refactor: add example for relative import

--- sampleproject/components/urls.py
+++ sampleproject/components/urls.py
@@ -1,8 +1,9 @@
-from components.calendar.calendar import Calendar
+from components.calendar.calendar import Calendar, CalendarRelative
 from components.greeting import Greeting
 from django.urls import path
 
 urlpatterns = [
     path("greeting/", Greeting.as_view(), name="greeting"),
     path("calendar/", Calendar.as_view(), name="calendar"),
+    path("calendar-relative/", CalendarRelative.as_view(), name="calendar-relative"),
 ]
