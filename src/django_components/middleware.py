b171eafea8e62014aacd807e46bf9ebaaedbf483
TheSteveBurgess <79967988+TheSteveBurgess@users.noreply.github.com>
2024-05-01 09:10:54 +0100

    Converted Middleware to Async

--- src/django_components/middleware.py
+++ src/django_components/middleware.py
@@ -6,8 +6,9 @@ from django.conf import settings
 from django.forms import Media
 from django.http import HttpRequest, HttpResponse, StreamingHttpResponse
 from django.http.response import HttpResponseBase
-
+from django.utils.decorators import sync_and_async_middleware
 from django_components.component_registry import registry
+from asgiref.sync import iscoroutinefunction, markcoroutinefunction
 
 if TYPE_CHECKING:
     from django_components.component import Component
@@ -25,6 +26,7 @@ PLACEHOLDER_REGEX = re.compile(
 )
 
 
+@sync_and_async_middleware
 class ComponentDependencyMiddleware:
     """Middleware that inserts CSS/JS dependencies for all rendered components at points marked with template tags."""
 
@@ -33,24 +35,50 @@ class ComponentDependencyMiddleware:
     def __init__(self, get_response: "Callable[[HttpRequest], HttpResponse]") -> None:
         self.get_response = get_response
 
+        if iscoroutinefunction(self.get_response):
+            markcoroutinefunction(self)
+
     def __call__(self, request: HttpRequest) -> HttpResponseBase:
+
+        if iscoroutinefunction(self):
+            return self.__acall__(request)
+
         response = self.get_response(request)
+        response = self.process_response(request)
+        return response
+
+    async def __acall__(self, request):
+
+        response = await self.get_response(request)
+        response = self.process_response(response)
+        return response
+
+    def process_response(self, response):
         if (
             getattr(settings, "COMPONENTS", {}).get("RENDER_DEPENDENCIES", False)
             and not isinstance(response, StreamingHttpResponse)
             and response.get("Content-Type", "").startswith("text/html")
         ):
             response.content = process_response_content(response.content)
+
         return response
 
 
 def process_response_content(content: bytes) -> bytes:
-    component_names_seen = {match.group("name") for match in COMPONENT_COMMENT_REGEX.finditer(content)}
-    all_components = [registry.get(name.decode("utf-8"))("") for name in component_names_seen]
+    component_names_seen = {
+        match.group("name") for match in COMPONENT_COMMENT_REGEX.finditer(content)
+    }
+    all_components = [
+        registry.get(name.decode("utf-8"))("") for name in component_names_seen
+    ]
     all_media = join_media(all_components)
     js_dependencies = b"".join(media.encode("utf-8") for media in all_media.render_js())
-    css_dependencies = b"".join(media.encode("utf-8") for media in all_media.render_css())
-    return PLACEHOLDER_REGEX.sub(DependencyReplacer(css_dependencies, js_dependencies), content)
+    css_dependencies = b"".join(
+        media.encode("utf-8") for media in all_media.render_css()
+    )
+    return PLACEHOLDER_REGEX.sub(
+        DependencyReplacer(css_dependencies, js_dependencies), content
+    )
 
 
 def add_module_attribute_to_scripts(scripts: str) -> str:
