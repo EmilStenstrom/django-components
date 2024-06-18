9a3eaa6008ff0b9b09cdea51ebf0b6874be4fff2
Lilian DUREY <dureylilian@gmail.com>
2024-06-18 09:23:45 +0200

    created component using Path as object with static

--- sampleproject/components/calendar/calendar.py
+++ sampleproject/components/calendar/calendar.py
@@ -1,4 +1,6 @@
 from django_components import component
+from django.utils.html import format_html, html_safe
+from django.templatetags.static import static
 
 
 @component.register("calendar")
@@ -47,3 +49,36 @@ class CalendarRelative(component.Component):
     class Media:
         css = "calendar.css"
         js = "calendar.js"
+
+
+@html_safe
+class PathObject:
+    def __init__(self, static_path):
+        self.static_path = static_path
+
+    def __str__(self):
+        return format_html(f'<script type="module" src="{static(self.static_path)}"></script>')
+
+
+@component.register("calendar_path_object")
+class CalendarPathObject(component.Component):
+    # Note that Django will look for templates inside `[your apps]/components` dir and
+    # `[project root]/components` dir. To customize which template to use based on context
+    # you can override def get_template_name() instead of specifying the below variable.
+    template_name = "calendar/calendar.html"
+
+    # This component takes one parameter, a date string to show in the template
+    def get_context_data(self, date):
+        return {
+            "date": date,
+        }
+
+    def get(self, request, *args, **kwargs):
+        context = {
+            "date": request.GET.get("date", ""),
+        }
+        return self.render_to_response(context)
+
+    class Media:
+        css = "calendar/calendar.css"
+        js = PathObject("calendar/calendar.js")
