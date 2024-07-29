869701c0ea8c65e6de3bb3a7bb9431b43c1bcd83
Juro Oravec <juraj.oravec.josefson@gmail.com>
2024-07-29 22:26:09 +0200

    refactor: remove unnecessary comments

--- src/django_components/__init__.py
+++ src/django_components/__init__.py
@@ -2,16 +2,16 @@
 import django
 
 # Public API
-from django_components.autodiscover import autodiscover as autodiscover  # NOQA
-from django_components.autodiscover import import_libraries as import_libraries  # NOQA
-from django_components.component import Component as Component  # NOQA
-from django_components.component_registry import AlreadyRegistered as AlreadyRegistered  # NOQA
-from django_components.component_registry import ComponentRegistry as ComponentRegistry  # NOQA
-from django_components.component_registry import NotRegistered as NotRegistered  # NOQA
-from django_components.component_registry import register as register  # NOQA
-from django_components.component_registry import registry as registry  # NOQA
-from django_components.types import css as css  # NOQA
-from django_components.types import django_html as django_html  # NOQA
+from django_components.autodiscover import autodiscover as autodiscover
+from django_components.autodiscover import import_libraries as import_libraries
+from django_components.component import Component as Component
+from django_components.component_registry import AlreadyRegistered as AlreadyRegistered
+from django_components.component_registry import ComponentRegistry as ComponentRegistry
+from django_components.component_registry import NotRegistered as NotRegistered
+from django_components.component_registry import register as register
+from django_components.component_registry import registry as registry
+from django_components.types import css as css
+from django_components.types import django_html as django_html
 from django_components.types import js as js
 
 if django.VERSION < (3, 2):
