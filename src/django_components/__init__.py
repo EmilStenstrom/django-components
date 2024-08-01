d1196bab30700288fb17943be967b9aa40fb0472
Juro Oravec <juraj.oravec.josefson@gmail.com>
2024-08-01 16:44:10 +0200

    refactor: fix linter error in exports

--- src/django_components/__init__.py
+++ src/django_components/__init__.py
@@ -2,6 +2,7 @@
 import django
 
 # Public API
+# isort: off
 from django_components.autodiscover import autodiscover as autodiscover
 from django_components.autodiscover import import_libraries as import_libraries
 from django_components.component import Component as Component
@@ -10,7 +11,9 @@ from django_components.component_registry import ComponentRegistry as ComponentR
 from django_components.component_registry import NotRegistered as NotRegistered
 from django_components.component_registry import register as register
 from django_components.component_registry import registry as registry
-from django_components import types as types
+import django_components.types as types
+
+# isort: on
 
 if django.VERSION < (3, 2):
     default_app_config = "django_components.apps.ComponentsConfig"
