521b2042a929271d462987638f954f6d999d6560
Juro Oravec <juraj.oravec.josefson@gmail.com>
2024-09-12 09:40:46 +0200

    refactor: build step for JS and include JS static files in package distribution

--- src/django_components_js/scripts/postbuild.js
+++ src/django_components_js/scripts/postbuild.js
@@ -5,7 +5,7 @@ const path = require('path');
 const main = async () => {
   // Make the built script available in Django as static file
   // See https://testdriven.io/blog/django-static-files/
-  const DEST_DIR = '../django_components/static/django_components';
+  const DEST_DIR = '../django_components/static/django_components/__generated__';
 
   await fsp.mkdir(DEST_DIR, { recursive: true });
   await fsp.copyFile('./dist/cdn.min.js', path.join(DEST_DIR, 'django_components.min.js'));
