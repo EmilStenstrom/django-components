1fd95708d1239a46d5de19af5aa319942648f54d
Juro Oravec <juraj.oravec.josefson@gmail.com>
2024-11-03 08:46:30 +0100

    docs: add docstrings (#732)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/django_components/util/types.py
+++ src/django_components/util/types.py
@@ -31,7 +31,113 @@ except ImportError:
 
 
 EmptyTuple = Tuple[()]
+"""
+Tuple with no members.
+
+You can use this to define a [Component](../api#django_components.Component)
+that accepts NO positional arguments:
+
+```python
+from django_components import Component, EmptyTuple
+
+class Table(Component(EmptyTuple, Any, Any, Any, Any, Any))
+    ...
+```
+
+After that, when you call [`Component.render()`](../api#django_components.Component.render)
+or [`Component.render_to_response()`](../api#django_components.Component.render_to_response),
+the `args` parameter will raise type error if `args` is anything else than an empty
+tuple.
+
+```python
+Table.render(
+    args: (),
+)
+```
+
+Omitting `args` is also fine:
+
+```python
+Table.render()
+```
+
+Other values are not allowed. This will raise an error with MyPy:
+
+```python
+Table.render(
+    args: ("one", 2, "three"),
+)
+```
+"""
 
 
 class EmptyDict(TypedDict):
+    """
+    TypedDict with no members.
+
+    You can use this to define a [Component](../api#django_components.Component)
+    that accepts NO kwargs, or NO slots, or returns NO data from
+    [`Component.get_context_data()`](../api#django_components.Component.get_context_data)
+    /
+    [`Component.get_js_data()`](../api#django_components.Component.get_js_data)
+    /
+    [`Component.get_css_data()`](../api#django_components.Component.get_css_data):
+
+    Accepts NO kwargs:
+
+    ```python
+    from django_components import Component, EmptyDict
+
+    class Table(Component(Any, EmptyDict, Any, Any, Any, Any))
+        ...
+    ```
+
+    Accepts NO slots:
+
+    ```python
+    from django_components import Component, EmptyDict
+
+    class Table(Component(Any, Any, EmptyDict, Any, Any, Any))
+        ...
+    ```
+
+    Returns NO data from `get_context_data()`:
+
+    ```python
+    from django_components import Component, EmptyDict
+
+    class Table(Component(Any, Any, Any, EmptyDict, Any, Any))
+        ...
+    ```
+
+    Going back to the example with NO kwargs, when you then call
+    [`Component.render()`](../api#django_components.Component.render)
+    or [`Component.render_to_response()`](../api#django_components.Component.render_to_response),
+    the `kwargs` parameter will raise type error if `kwargs` is anything else than an empty
+    dict.
+
+    ```python
+    Table.render(
+        kwargs: {},
+    )
+    ```
+
+    Omitting `kwargs` is also fine:
+
+    ```python
+    Table.render()
+    ```
+
+    Other values are not allowed. This will raise an error with MyPy:
+
+    ```python
+    Table.render(
+        kwargs: {
+            "one": 2,
+            "three": 4,
+        },
+    )
+    ```
+    """
+
     pass
