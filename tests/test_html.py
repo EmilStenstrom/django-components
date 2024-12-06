e4b001d2bb87d72f8dbb3bfaf91107a65c53b6c0
Juro Oravec <juraj.oravec.josefson@gmail.com>
2024-12-06 14:37:21 +0100

    refactor: add tests for html parser impl

--- tests/test_html.py
+++ tests/test_html.py
@@ -1,23 +1,31 @@
-from typing import List, cast
-
 from django.test import TestCase
-from selectolax.lexbor import LexborHTMLParser, LexborNode
 
-from django_components.util.html import (
-    is_html_parser_fragment,
-    parse_document_or_nodes,
-    parse_multiroot_html,
-    parse_node,
-)
-
+from django_components.util.html import SoupNode
 from .django_test_setup import setup_test_config
 
 setup_test_config({"autodiscover": False})
 
 
 class HtmlTests(TestCase):
-    def test_parse_node(self):
-        node = parse_node(
+    def test_beautifulsoup_impl(self):
+        nodes = SoupNode.from_fragment(
+            """
+            <div class="abc xyz" data-id="123">
+                <ul>
+                    <li>Hi</li>
+                </ul>
+            </div>
+            <!-- I'M COMMENT -->
+            <button>
+                Click me!
+            </button>
+            """
+        )
+
+        self.assertEqual(len(nodes), 3)
+
+        self.assertHTMLEqual(
+            nodes[0].to_html(),
             """
             <div class="abc xyz" data-id="123">
                 <ul>
@@ -26,242 +34,73 @@ class HtmlTests(TestCase):
             </div>
             """
         )
-        node.attrs["id"] = "my-id"  # type: ignore[index]
-        node.css("li")[0].attrs["class"] = "item"  # type: ignore[index]
-
         self.assertHTMLEqual(
-            node.html,
-            """
-            <div class="abc xyz" data-id="123" id="my-id">
-                <ul>
-                    <li class="item">Hi</li>
-                </ul>
-            </div>
-            """,
-        )
-
-    def test_parse_multiroot_html(self):
-        html = """
-            <div class="abc xyz" data-id="123">
-                <ul>
-                    <li>Hi</li>
-                </ul>
-            </div>
-            <main id="123" class="one">
-                <div>
-                    42
-                </div>
-            </main>
-            <span>
-                Hello
-            </span>
-        """
-        nodes = parse_multiroot_html(html)
-
-        self.assertHTMLEqual(
-            nodes[0].html,
-            """
-            <div class="abc xyz" data-id="123">
-                <ul>
-                    <li>Hi</li>
-                </ul>
-            </div>
-            """,
+            nodes[1].to_html(),
+            "<!-- I'M COMMENT -->",
         )
         self.assertHTMLEqual(
-            nodes[1].html,
+            nodes[2].to_html(),
             """
-            <main id="123" class="one">
-                <div>
-                    42
-                </div>
-            </main>
-            """,
-        )
-        self.assertHTMLEqual(
-            nodes[2].html,
+            <button>
+                Click me!
+            </button>
             """
-            <span>
-                Hello
-            </span>
-            """,
         )
 
-    def test_is_html_parser_fragment(self):
-        fragment_html = """
-            <div class="abc xyz" data-id="123">
-                <ul>
-                    <li>Hi</li>
-                </ul>
-            </div>
-            <main id="123" class="one">
-                <div>
-                    42
-                </div>
-            </main>
-            <span>
-                Hello
-            </span>
-        """
-        fragment_tree = LexborHTMLParser(fragment_html)
-        fragment_result = is_html_parser_fragment(fragment_html, fragment_tree)
+        self.assertEqual(nodes[0].name(), "div")
+        self.assertEqual(nodes[2].name(), "button")
 
-        self.assertEqual(fragment_result, True)
+        self.assertEqual(nodes[0].is_element(), True)
+        self.assertEqual(nodes[1].is_element(), False)
+        self.assertEqual(nodes[2].is_element(), True)
 
-        doc_html = """
-            <!doctype html>
-            <html>
-              <head>
-                <link href="https://..." />
-              </head>
-              <body>
-                <div class="abc xyz" data-id="123">
-                    <ul>
-                        <li>Hi</li>
-                    </ul>
-                </div>
-              </body>
-            </html>
-        """
-        doc_tree = LexborHTMLParser(doc_html)
-        doc_result = is_html_parser_fragment(doc_html, doc_tree)
+        self.assertEqual(nodes[0].get_attr("class"), "abc xyz")
+        self.assertEqual(nodes[2].get_attr("class"), None)
 
-        self.assertEqual(doc_result, False)
+        nodes[0].set_attr("class", "123 456")
+        nodes[2].get_attr("class", "abc def")
+        self.assertEqual(nodes[0].get_attr("class"), "123 456")
+        self.assertEqual(nodes[2].get_attr("class"), "abc def")
 
-    def test_parse_document_or_nodes__fragment(self):
-        fragment_html = """
-            <div class="abc xyz" data-id="123">
-                <ul>
-                    <li>Hi</li>
-                </ul>
-            </div>
-            <main id="123" class="one">
-                <div>
-                    42
-                </div>
-            </main>
-            <span>
-                Hello
-            </span>
-        """
-        fragment_result = cast(List[LexborNode], parse_document_or_nodes(fragment_html))
-
+        # Setting attr to `True` will set it to boolean attribute,
+        # while setting it to `False` will remove the attribute.
+        nodes[2].set_attr("disabled", True)
         self.assertHTMLEqual(
-            fragment_result[0].html,
+            nodes[2].to_html(),
             """
-            <div class="abc xyz" data-id="123">
-                <ul>
-                    <li>Hi</li>
-                </ul>
-            </div>
-            """,
+            <button disabled>
+                Click me!
+            </button>
+            """
         )
+        nodes[2].set_attr("disabled", False)
         self.assertHTMLEqual(
-            fragment_result[1].html,
+            nodes[2].to_html(),
             """
-            <main id="123" class="one">
-                <div>
-                    42
-                </div>
-            </main>
-            """,
-        )
-        self.assertHTMLEqual(
-            fragment_result[2].html,
+            <button>
+                Click me!
+            </button>
             """
-            <span>
-                Hello
-            </span>
-            """,
         )
 
-    def test_parse_document_or_nodes__mixed(self):
-        fragment_html = """
-            <link href="" />
-            <div class="abc xyz" data-id="123">
-                <ul>
-                    <li>Hi</li>
-                </ul>
-            </div>
-            <main id="123" class="one">
-                <div>
-                    42
-                </div>
-            </main>
-            <span>
-                Hello
-            </span>
-        """
-        fragment_result = cast(List[LexborNode], parse_document_or_nodes(fragment_html))
+        # Return self
+        self.assertEqual(nodes[0].node, nodes[0].find_tag("div").node)
+        # Return descendant
+        li = nodes[0].find_tag("li")
+        self.assertHTMLEqual(li.to_html(), "<li>Hi</li>")
+        # Return None when not found
+        self.assertEqual(nodes[0].find_tag("main"), None)
 
+        # Insert children
+        li.append_children(nodes[2])
         self.assertHTMLEqual(
-            fragment_result[0].html,
+            li.to_html(),
             """
-            <link href="" />
-            """,
-        )
-        self.assertHTMLEqual(
-            fragment_result[1].html,
-            """
-            <div class="abc xyz" data-id="123">
-                <ul>
-                    <li>Hi</li>
-                </ul>
-            </div>
-            """,
-        )
-        self.assertHTMLEqual(
-            fragment_result[2].html,
-            """
-            <main id="123" class="one">
-                <div>
-                    42
-                </div>
-            </main>
-            """,
-        )
-        self.assertHTMLEqual(
-            fragment_result[3].html,
-            """
-            <span>
-                Hello
-            </span>
-            """,
-        )
-
-    def test_parse_document_or_nodes__doc(self):
-        doc_html = """
-            <!doctype html>
-            <html>
-              <head>
-                <link href="https://..." />
-              </head>
-              <body>
-                <div class="abc xyz" data-id="123">
-                    <ul>
-                        <li>Hi</li>
-                    </ul>
-                </div>
-              </body>
-            </html>
-        """
-        fragment_result = cast(LexborHTMLParser, parse_document_or_nodes(doc_html))
-
-        self.assertHTMLEqual(
-            fragment_result.html,
-            """
-            <!doctype html>
-            <html>
-              <head>
-                <link href="https://..." />
-              </head>
-              <body>
-                <div class="abc xyz" data-id="123">
-                    <ul>
-                        <li>Hi</li>
-                    </ul>
-                </div>
-              </body>
-            </html>
+            <li>
+                Hi
+                <button>
+                    Click me!
+                </button>
+            </li>
             """,
         )
